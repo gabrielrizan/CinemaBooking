6d60e70da8bffcb11551678ede0074aeac8b7079
Gabriel <rizangabriel3@gmail.com>
2025-01-11 20:39:48 +0200

    changed viewport of the container

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,11 +3,15 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
-    selector: 'app-footer',
-    imports: [CardModule, ButtonModule],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.css'
+  selector: 'app-footer',
+  imports: [CardModule, ButtonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  currentYear = new Date().getFullYear();
 
+  constructor() {}
+
+  ngOnInit() {}
 }
